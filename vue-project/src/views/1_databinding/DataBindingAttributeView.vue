b28e096c80ff20f0f675cff3dbeff5b9655f47ab
unknown <lghee282@gmail.com>
2022-07-20 02:48:29 +0900

    22/07/20

---
+++ vue-project/src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" name="" id="" v-bind:value="userId" readonly />
+    <input type="text" name="" id="" :value="userId" readonly />
+    <br>
+    <img :src="imgSrc" alt="" style="width:100px; height:auto;">
+    <br>
+    <input type="search" name="" id="" v-model="txt1" />
+    <button :disabled="txt1 === ''">조회</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: 'seungwongo',
+      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png',
+      txt1: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
