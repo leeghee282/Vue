18f182c6ca9fa31baa335d9359ccc146011d1a8b
unknown <lghee282@gmail.com>
2022-07-21 22:23:55 +0900

    22/07/21

---
+++ vue-project/src/views/1_databinding/DataBindingClassView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div :class="{'text-red': true}">클래스 바인딩</div>
+    <div :class="{'text-red': false}">클래스 바인딩</div>
+    <div :class="{'text-red': true, active: true}">클래스 바인딩</div>
+    <div :class="{'text-red': hasError, active: isActive}">클래스 바인딩</div>
+    <br>
+    <div :class="class2">클래스 바인딩2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+      hasError: false,
+      class2: ['active', 'hasError']
+    }
+  }
+}
+</script>
+
+<style>
+.active {
+  background-color: greenyellow;
+  font-weight: bold;
+}
+
+.text-red {
+  color: red;
+}
+</style>
